7feccc1a72214cbe38da2d2f1a427409dfe38367
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-06 12:32:31 -0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,10 +1,9 @@
 _G.Name = game.Players.LocalPlayer.Name
 if getgenv().isLoaded then return end
-if _G.Name == "911hz" or _G.Name == "911hw" then
+if _G.Name == "911hz" or _G.Name == "911hw" or _G.Name == "911hl" or _G.Name == "911ht" or _G.Name == "911hn" or _G.Name == "911hb" or _G.Name == "911h3" or _G.Name == "icanmodz" or _G.Name == "HowdyMcShuffle" or _G.Name == "HowdyMcShuffle1" or _G.Name == "HowdyMcShuffle2" or _G.Name == "HowdyMcShuffle3" or _G.Name == "HowdyMcShuffle4" or _G.Name == "HowdyMcShuffle5" or _G.Name == "HowdyMcShuffle6" then
     game:GetService("UserInputService").InputBegan:connect(function(inputObject,gameProcessedEvent)
         if inputObject.KeyCode == Enum.KeyCode.W then
             if getgenv().isLoaded then return end
-            print('you have been verified')
             loadstring(game:HttpGet("https://pastebin.com/raw/ntDzaSdL", true))()
             getgenv().isLoaded = true
         end
