03bf06c6baf84114e37ef2c2140910ab87c8e807
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-07 16:26:27 -0500

    Delete HWID Checker.lua

--- HWID Checker.lua
+++
@@ -1,7 +0,0 @@
-local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/HWID%20LIST.lua"))()
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-for i,v in pair(HWIDTable) do
-    if v == HWID then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/SecondPiece_XHub.lua?token=GHSAT0AAAAAACOXAQUOGR3IKS7JBHYUF3FGZO35SJA"))()
-    end
-end
