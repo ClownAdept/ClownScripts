6dc4146411d890829884d3d325c1d227c2f7d857
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-07 17:05:01 -0500

    Update HWID-Checker.lua

--- HWID-Checker.lua
+++ HWID-Checker.lua
@@ -1,6 +1,7 @@
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 local hwidlist = loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/Whitelist-HWIDLIST.lua"))()
 
+wait(5)
 for i, v in pairs(hwidlist) do
     if v == HWID then
         print("whitelisted")
