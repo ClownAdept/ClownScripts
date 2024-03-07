2ef1929e187cc2ff2fddb6e251a9e90889cf96d6
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-07 02:54:43 -0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,6 +1,6 @@
 _G.Name = game.Players.LocalPlayer.Name
 if getgenv().isLoaded then return end
-if _G.Name == "911hz" or _G.Name == "911hw" or _G.Name == "911hl" or _G.Name == "911ht" or _G.Name == "911hn" or _G.Name == "911hb" or _G.Name == "911h3" or _G.Name == "icanmodz" or _G.Name == "HowdyMcShuffle" or _G.Name == "HowdyMcShuffle1" or _G.Name == "HowdyMcShuffle2" or _G.Name == "HowdyMcShuffle3" or _G.Name == "HowdyMcShuffle4" or _G.Name == "HowdyMcShuffle5" or _G.Name == "HowdyMcShuffle6" or _G.Name == "TheRejectdead" or _G.Name == "meowsers329" or _G.Name == "meowsers330" then
+if _G.Name == "911hz" or _G.Name == "911hw" or _G.Name == "911hl" or _G.Name == "911ht" or _G.Name == "911hn" or _G.Name == "911hb" or _G.Name == "911h3" or _G.Name == "ICanModz" or _G.Name == "HowdyMcShuffle" or _G.Name == "HowdyMcShuffle1" or _G.Name == "HowdyMcShuffle2" or _G.Name == "HowdyMcShuffle3" or _G.Name == "HowdyMcShuffle4" or _G.Name == "HowdyMcShuffle5" or _G.Name == "HowdyMcShuffle6" or _G.Name == "TheRejectdead" or _G.Name == "meowsers329" or _G.Name == "meowsers330" then
     game:GetService("UserInputService").InputBegan:connect(function(inputObject,gameProcessedEvent)
         if inputObject.KeyCode == Enum.KeyCode.W then
             if getgenv().isLoaded then return end
