af838bf724d46a6975e9dc56fc3a3e890ef283f2
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-07 17:22:11 -0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/Webhook.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/SecondPiece_XHub.lua?token=GHSAT0AAAAAACOXAQUOGR3IKS7JBHYUF3FGZO35SJA"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/HWID-Checker.lua"))()
