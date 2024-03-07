094e6ccabf4b6d1c5f34c629cc8a7828af6f55c2
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-07 16:44:11 -0500

    Create HWID-Checker.lua

---
+++ HWID-Checker.lua
@@ -0,0 +1,11 @@
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+local hwidlist = loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/Whitelist-HWIDLIST.lua"))()
+
+for i, v in pairs(hwidlist) do
+    if v == HWID then
+        print("whitelisted")
+    else
+        print("not whitelisted")
+        game.Players.LocalPlayer:Kick("You are Not Whitelisted, Join the Discord Server (https://discord.gg/HZQE9kZbGx), or DM me on discord clown.hz  Sorry!")
+    end
+end
