bde5fd8ae4ae35da9a97bfdce3b0175fc2b246b2
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-08 19:35:56 -0500

    Update GameID-HUBLoader.lua

--- GameID-HUBLoader.lua
+++ GameID-HUBLoader.lua
@@ -1,1 +1,9 @@
-
+if game.PlaceId == 15049111150 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/SecondPiece_XHub.lua?token=GHSAT0AAAAAACOXAQUOGR3IKS7JBHYUF3FGZO35SJA"))()
+end
+if game.PlaceId == 914010731 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/RoGhoul_RGK_Premium.lua"))()
+end
+if game.PlaceId == 4023445890 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ClownAdept/ClownScripts/main/BizzareAdventure_ClownXphoze.lua"))()
+end
