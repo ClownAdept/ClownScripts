7e126039e0570871dfa5db9ef9473f170a972176
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-03-11 18:23:49 -0400

    Update SecondPiece_XHub.lua

--- SecondPiece_XHub.lua
+++ SecondPiece_XHub.lua
@@ -4879,11 +4879,8 @@ end)
 a7:Button("Server Hop", function()
 local Http = game:GetService("HttpService") local TPS = game:GetService("TeleportService") local Api = "https://games.roblox.com/v1/games/" local _place = game.PlaceId local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100" function ListServers(cursor) local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or "")) return Http:JSONDecode(Raw) end local Server, Next; repeat local Servers = ListServers(Next) Server = Servers.data[1] Next = Servers.nextPageCursor until Server TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
 end)
-
-else
-print("smth")
 end
 else
-print("smth")
+game:GetService("Players").LocalPlayer:Kick("KEY INCORRECT")
      end
 end)
