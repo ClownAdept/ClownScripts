1c1b4cb53ad155ef647c969e881adf3c504f00f7
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-06-27 17:34:45 -0400

    Update SecondPiece_XHub.lua

--- SecondPiece_XHub.lua
+++ SecondPiece_XHub.lua
@@ -4292,10 +4292,11 @@ end)
     pcall(function()
     if not game.Workspace.Lives:FindFirstChild("Criminal") then
     repeat task.wait()
-      for i,v in pairs(game:GetService("Workspace").NPC.Kuru:GetDescendants()) do
-        if v.ClassName == "ProximityPrompt" then
-        fireproximityprompt(v,30)
-        wait(1)
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-911.711487, 41.7252045, 1114.65466, 0.9993909, 0.00116581446, -0.0348785296, 1.34973668e-08, 0.999441862, 0.0334067158, 0.0348980092, -0.0333863683, 0.99883312)
+      wait(1)
+      game:service('VirtualInputManager'):SendKeyEvent(true, "E", false, game)
+      wait(1)
+      game:service('VirtualInputManager'):SendKeyEvent(false, "E", false, game)
     until _G.AutobQuest == false or game.Workspace.Lives:FindFirstChild("Criminal")
     end
     end)
