37e228d2f3a47758fb18c666caae08b75db3b2f5
Clown <139822699+ClownAdept@users.noreply.github.com>
2024-12-06 01:37:39 -0500

    Update SPworking.lua

--- SPworking.lua
+++ SPworking.lua
@@ -4037,6 +4037,7 @@ getgenv().Configs = {
           for i,v in pairs(game:GetService("Workspace").Items:GetDescendants()) do
             if v.ClassName == "ProximityPrompt" then
               fireproximityprompt(v,30)
+              wait(1)
       
               -- Calculate the tween time based on the distance and speed
               local speed = getgenv().Speed or 1000 -- Default speed if not defined
